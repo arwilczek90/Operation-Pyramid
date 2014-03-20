a48777934a2478edc6d4bb82cd819b9e90547ed1
Adam Wilczek <arwilczek90@gmail.com>
2014-03-20 16:47:23 -0500

    Added get time function to clock created info class with some base attributes. added a get info class to car.

--- src/SelfDrivingCar/Car.java
+++ src/SelfDrivingCar/Car.java
@@ -4,4 +4,17 @@ package SelfDrivingCar;
  * Created by awilczek on 3/18/14.
  */
 public class Car {
+
+    public Car() {
+
+    }
+
+    public Info getInfo()
+    {
+        Info ret = new Info();
+
+        return ret;
+    }
+
+
 }
