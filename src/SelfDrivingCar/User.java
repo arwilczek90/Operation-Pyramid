89af2d892f51372d6706df9e9659e6012ae269d6
gmogne <gmogne@gmail.com>
2014-03-25 16:45:45 -0500

    Added variables and methods for User as shown in uml design.

--- src/SelfDrivingCar/User.java
+++ src/SelfDrivingCar/User.java
@@ -4,4 +4,24 @@ package SelfDrivingCar;
  * Created by awilczek on 3/18/14.
  */
 public class User {
+    private String name = "Default User";
+    private Boolean drivePermission = false;
+    private Boolean windowPermission = true;
+    private Zone passengerPref;
+    private Zone driverPref;
+
+    public void updateDriverPref(Zone currentSettings){
+        // if currentSettings.type = driver
+        driverPref = currentSettings;
+    }
+
+    public void updatePassengerPref(Zone currentSettings){
+        // if currentSettings.type = passenger
+        passengerPref = currentSettings;
+    }
+
+    public void setPermissions(boolean newDrive, boolean newWindow){
+        windowPermission = newWindow;
+        drivePermission = newDrive;
+    }
 }
