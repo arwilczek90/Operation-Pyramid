e783ba5e8501b4307ff2b7aed50cd853937d3e61
Chris Wilcoxson <chris@slushman.com>
2014-03-25 16:46:09 -0500

    * Added Javadocs for class variables and methods. * Completed setStatus and setLocation methods

--- src/SelfDrivingCar/Door.java
+++ src/SelfDrivingCar/Door.java
@@ -5,29 +5,55 @@ package SelfDrivingCar;
  */
 public class Door {
 
+    /**
+     * The status of the door: locked, closed, or open
+     */
     private String status;
+
+    /**
+     * The location of the door within the car: driver or passenger
+     */
     private String location;
 
+    /**
+     * Return status class variable
+     *
+     * @return  status class variable
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Returns Location class variable
+     *
+     * @return  location class variable
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Sets Status class variable
+     *
+     * @param   doorStatus
+     */
     public void setStatus(String doorStatus) {
-        /*
-        if ( "open" == doorStatus) {
-            this.status = "open";
-        } else if( "locked" == doorStatus ) {
-            this.status = "locked";
-        } else {
-            this.status = "closed";
-        }*/
+        // if doorStatus is not open, closed, or locked, return
+        if ( doorStatus.equals("open") || doorStatus.equals("locked") || doorStatus.equals("closed") ) { return; }
+        // otherwise, set status as doorStatus
+        this.status = doorStatus;
     }
 
+    /**
+     * Sets Location class variable
+     *
+     * @param   doorLocation
+     */
     public void setLocation(String doorLocation) {
+        // if doorLocation is not driver or passenger, return
+        if ( doorLocation.equals("driver") || doorLocation.equals("passenger") ) { return; }
+        // otherwise set location as doorLocation
         this.location = doorLocation;
     }
 
