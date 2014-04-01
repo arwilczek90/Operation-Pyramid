c44c64f791d210ed92e976489ffee37fad4aa892
g0sh <joshua.gieringer@gmail.com>
2014-04-01 16:49:02 -0500

    Updated Drive Modes Manual and Auto

--- src/SelfDrivingCar/AutoDrive.java
+++ src/SelfDrivingCar/AutoDrive.java
@@ -4,4 +4,32 @@ package SelfDrivingCar;
  * Created by awilczek on 3/18/14.
  */
 public class AutoDrive {
+    private boolean autoD;
+    private boolean displayNav;
+    //private Navigation dest; Waiting for Navigation class to be comepleted
+
+    public void AutoDrive() {
+        autoD = true; //
+    }
+
+    public boolean isAuto() { //for User class to use checking if in Auto Drive
+        return autoD;
+    }
+
+    public void setDriveMode(boolean autoD) {
+        this.autoD = autoD;
+    }
+
+    public void navTo(Navigation dest){ //take destination
+        //this.dest = dest;
+        //waiting for Navigation class to be completed
+    }
+
+    public boolean navStatus() { //returns Navigation Status to show Nav or not
+        return displayNav;
+    }
+
+    public void setNavStat(boolean x) { //to display, or not to display Nav
+        displayNav = x;
+    }
 }
--- src/SelfDrivingCar/ManualDrive.java
+++ src/SelfDrivingCar/ManualDrive.java
@@ -4,4 +4,33 @@ package SelfDrivingCar;
  * Created by awilczek on 3/18/14.
  */
 public class ManualDrive {
+    private boolean manD;
+    private boolean displayNav; //for Car to use whether to keep navigation on/off
+    public void ManualDrive() {
+        manD = true; //manual drive is engaged
+        displayNav = true; //Navigation is viewable
+    }
+
+    public boolean isMan() { //for User class to use checking if in Auto Drive
+        return manD;
+    }
+
+    public void setDriveMode(boolean manD) {
+        this.manD = manD;
+    }
+
+    public void navTo(Navigation dest){ //take destination
+        //Navigation destination = dest; Waiting for Navigation class to be comepleted
+        //waiting for Navigation class to be completed
+    }
+
+    public boolean navStatus() { //returns Navigation Status to show Nav or not
+        return displayNav;
+    }
+
+    public void setNavStat(boolean x) { //to display, or not to display Nav
+        displayNav = x;
+    }
+
+
 }
