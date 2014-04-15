e49df74b51a6f9f1a7e3ad019249caf507d6c3ea
gmogne <gmogne@users.noreply.github.com>
2014-04-15 16:40:34 -0500

    Update AutoDrive.java

--- src/SelfDrivingCar/AutoDrive.java
+++ src/SelfDrivingCar/AutoDrive.java
@@ -88,19 +88,9 @@ private void stop(){
 
 
 
-/*public void turn(int angle){}
-public void speed(int speed){}
-public void park(){}
-public void intersection(){}
-*/
-
-//private void accelerate(){}
 private int getGear(){return 1;}
 private void turnWheels(){}
 private void setGear(int gear){}
-//private void decelerate(){}
-//private void stop(){}
-//public int getSpeed(){return 100;}
 private void setSpeed(int speed){}
 private boolean checkAhead(){return false;}
 private boolean checkSides(){return false;}
